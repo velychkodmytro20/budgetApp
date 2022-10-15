778fc623dfe73b191bc724245bd48ac15ce5f500
Dmytro Velychko <ilovenunas@gmail.com>
2022-10-15 11:04:59 +0300

    AT-2 refactor

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,23 +1,22 @@
 export interface CreateUserDto {
-  id?: string
-  firstName: string
-  lastName: string
-  password: string
-  email: string
-  phone?: string | null
-  status?: Status
-  role?: Role
-  lastLoginAt?: Date | string | null
-  createdAT?: Date | string | null
-  updatedAt?: Date | string | null
-  //   budgetRecord?: BudgetRecordCreateNestedManyWithoutUserInput
+    id?: string
+    firstName: string
+    lastName: string
+    password: string
+    email: string
+    phone?: string | null
+    status?: Status
+    role?: Role
+    lastLoginAt?: Date | string | null
+    createdAT?: Date | string | null
+    updatedAt?: Date | string | null
 }
 
 enum Status {
-  active,
-  inactive,
+    active,
+    inactive,
 }
 enum Role {
-  user,
-  admin,
+    user,
+    admin,
 }
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,107 +1,118 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Put,
-  Param,
-  Delete,
-  Query,
-  Logger,
-  HttpException,
-  HttpStatus,
+    Controller,
+    Get,
+    Post,
+    Body,
+    Put,
+    Param,
+    Delete,
+    Query,
+    Logger,
+    HttpException,
+    HttpStatus,
+    UseGuards,
 } from '@nestjs/common'
-import { UserService } from './user.service'
 import { User, Prisma } from '@prisma/client'
+
+import { JwtAuthGuard } from './../auth/jwt-auth.guard'
+import { UserService } from './user.service'
+import { JwtStrategy } from '../auth/jwt.strategy'
 @Controller('user/')
 export class UserController {
-  private readonly logger: Logger
-  constructor(private readonly userService: UserService) {
-    this.logger = new Logger()
-  }
-
-  @Post('create')
-  async userCreate(@Body() data: Prisma.UserCreateInput): Promise<User> {
-    try {
-      const createdUser = await this.userService.create(data)
-      this.logger.debug('User was created', createdUser)
-
-      return createdUser
-    } catch (error) {
-      this.logger.error('Error while creating a user', error.message)
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
+    private readonly logger: Logger
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtStrategy: JwtStrategy,
+    ) {
+        this.logger = new Logger()
     }
-  }
 
-  @Get(':id')
-  async userfindOne(@Param('id') id: string): Promise<User> {
-    try {
-      const user = await this.userService.findOne({ id })
+    @Post('create')
+    @UseGuards(JwtAuthGuard)
+    async userCreate(@Body() data: Prisma.UserCreateInput): Promise<User> {
+        try {
+            const createdUser = await this.userService.create(data)
+            this.logger.debug('User was created', createdUser)
 
-      this.logger.debug('User was found in db', user)
-      return user
-    } catch (error) {
-      this.logger.error('Error while find a user', error.message)
-      throw new HttpException(error.message, HttpStatus.NOT_FOUND)
+            return createdUser
+        } catch (error) {
+            this.logger.error('Error while creating a user', error.message)
+            throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
+        }
     }
-  }
 
-  @Get()
-  async userFindManyByCriterias(
-    @Query() searchQuery: Prisma.UserWhereInput,
-  ): Promise<User[]> {
-    try {
-      const users = await this.userService.findMany(searchQuery)
+    @Get(':id')
+    async userfindOne(@Param('id') id: string): Promise<User> {
+        try {
+            const user = await this.userService.findOne({ id })
+            this.logger.debug('User was found in db', user)
 
-      return users
-    } catch (error) {
-      this.logger.error('Error while finding users', error.message)
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
+            return user
+        } catch (error) {
+            this.logger.error('Error while find a user', error.message)
+            throw new HttpException(error.message, HttpStatus.NOT_FOUND)
+        }
     }
-  }
 
-  @Put(':id')
-  async userUpdate(
-    @Body() data: Prisma.UserCreateInput,
-    @Param('id') id: string,
-  ): Promise<User> {
-    try {
-      const updatedUser = await this.userService.update({ data, id })
+    @Get()
+    async userFindManyByCriterias(
+        @Query() searchQuery: Prisma.UserWhereInput,
+    ): Promise<User[]> {
+        try {
+            const users = await this.userService.findMany(searchQuery)
 
-      return updatedUser
-    } catch (error) {
-      this.logger.error('Error while updating user', error.message)
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
+            return users
+        } catch (error) {
+            this.logger.error('Error while finding users', error.message)
+            throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
+        }
     }
-  }
 
-  @Put()
-  async userUpdateMany(
-    @Body() data: Prisma.UserCreateInput,
-    @Query() searchQuery: Prisma.UserWhereUniqueInput,
-  ): Promise<Prisma.BatchPayload> {
-    try {
-      const countOfUpdetedUsers = await this.userService.updateMany({
-        data,
-        searchQuery,
-      })
+    @Put(':id')
+    @UseGuards(JwtAuthGuard)
+    async userUpdate(
+        @Body() data: Prisma.UserCreateInput,
+        @Param('id') id: string,
+    ): Promise<User> {
+        try {
+            const updatedUser = await this.userService.update({ data, id })
 
-      return countOfUpdetedUsers
-    } catch (error) {
-      this.logger.error('Error while updating users', error.message)
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
+            return updatedUser
+        } catch (error) {
+            this.logger.error('Error while updating user', error.message)
+            throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
+        }
     }
-  }
 
-  @Delete(':id')
-  async userRemove(@Param('id') id: string): Promise<User> {
-    try {
-      const deletedUser = await this.userService.remove(id)
+    @Put()
+    @UseGuards(JwtAuthGuard)
+    async userUpdateMany(
+        @Body() data: Prisma.UserCreateInput,
+        @Query() searchQuery: Prisma.UserWhereUniqueInput,
+    ): Promise<Prisma.BatchPayload> {
+        try {
+            const countOfUpdetedUsers = await this.userService.updateMany({
+                data,
+                searchQuery,
+            })
 
-      return deletedUser
-    } catch (error) {
-      this.logger.error('Error while deleting a user', error.message)
-      throw new HttpException(error.message, HttpStatus.NOT_FOUND)
+            return countOfUpdetedUsers
+        } catch (error) {
+            this.logger.error('Error while updating users', error.message)
+            throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
+        }
     }
-  }
+
+    @Delete(':id')
+    @UseGuards(JwtAuthGuard)
+    async userRemove(@Param('id') id: string): Promise<User> {
+        try {
+            const deletedUser = await this.userService.remove(id)
+
+            return deletedUser
+        } catch (error) {
+            this.logger.error('Error while deleting a user', error.message)
+            throw new HttpException(error.message, HttpStatus.NOT_FOUND)
+        }
+    }
 }
